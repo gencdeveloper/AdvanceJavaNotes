8ab973b1d0fa1b7f9f902f6119e630cac2d1b036
Emre <yunusozudogru34@gmail.com>
2023-08-17 21:26:47 +0200

    Strean

---
+++ streams/src/main/java/AdvanceJavaNotes/Course.java
@@ -0,0 +1,10 @@
+package AdvanceJavaNotes;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+@Data
+@AllArgsConstructor
+public class Course {
+    private String courseName;
+    private Integer courseID;
+}
