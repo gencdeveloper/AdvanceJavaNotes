74910638e09d1213e87269c907f5845a403e01ab
Emre <yunusozudogru34@gmail.com>
2023-08-22 20:15:29 +0200

    fibonacci

---
+++ streams/src/main/java/AdvanceJavaNotes/InfiniteStream.java
@@ -0,0 +1,31 @@
+package AdvanceJavaNotes;
+
+import java.util.stream.Stream;
+
+public class InfiniteStream {
+    public static void main(String[] args) {
+        //This is rule
+        Stream.iterate(0,n->n+2)
+                .limit(5)
+                .forEach(System.out::println);
+
+
+        System.out.println("*******fibonacci series question*******");
+        //How does it work? iterate needs a lambda to specify the successor element.
+        //In the case of the tuple(3,5) the successor (5, 3+5) = (5,8)
+        //The next one (8, 5+8)
+
+        Stream.iterate(new int[]{0,1},t -> new int[]{t[1],t[0]+t[1]})
+                .limit(8)
+                .forEach(t -> System.out.println("(" +t[0] + "," +t[1] + ")"));
+
+
+        System.out.println("*******fibonacci series questions******");
+        Stream.iterate(new int[]{0,1},t -> new int[]{t[1],t[0]+t[1]})
+                .limit(8)
+                .map(t -> t[0])
+                .forEach(System.out::println);
+
+
+    }
+}
